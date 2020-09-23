459e25db419bb77fb6d046abfd106644a9fdde8b
Jake Heft <jakeheft@gmail.com>
2020-09-23 10:12:33 -0600

    Add ingredients method

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -1,11 +1,17 @@
 class CookBook
   attr_reader :recipes
-  
+
   def initialize
     @recipes = []
   end
 
   def add_recipe(recipe)
     @recipes << recipe
+  end
+
+  def ingredients
+    @recipes.flat_map do |recipe|
+      recipe.ingredients.map { |ingredient| ingredient.name }
+    end.uniq
   end
 end
