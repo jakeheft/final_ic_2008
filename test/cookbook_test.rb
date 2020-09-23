02e190572c8c015d07d0f16dee90a19c3b14a03a
Jake Heft <jakeheft@gmail.com>
2020-09-23 10:09:50 -0600

    Add test for ingredients

--- test/cookbook_test.rb
+++ test/cookbook_test.rb
@@ -15,11 +15,14 @@ class CookBookTest < Minitest::Test
     @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
     @recipe1 = Recipe.new("Mac and Cheese")
     @recipe2 = Recipe.new("Cheese Burger")
+
     @recipe1.add_ingredient(@ingredient1, 2)
     @recipe1.add_ingredient(@ingredient2, 8)
     @recipe2.add_ingredient(@ingredient1, 2)
     @recipe2.add_ingredient(@ingredient3, 4)
     @recipe2.add_ingredient(@ingredient4, 1)
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
   end
 
   def test_it_exists
@@ -27,8 +30,10 @@ class CookBookTest < Minitest::Test
   end
 
   def test_it_can_add_recipes
-    @cookbook.add_recipe(@recipe1)
-    @cookbook.add_recipe(@recipe2)
     assert_equal [@recipe1, @recipe2], @cookbook.recipes
+  end
+
+  def test_it_can_ingredients
+    assert_equal ["Cheese", "Macaroni", "Ground Beef", "Bun"], @cookbook.ingredients
   end
 end
